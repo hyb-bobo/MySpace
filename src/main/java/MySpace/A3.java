46b110fa0cbf44ad92acc231f80d8922b74f4501
黄小波 <cqyc520@126.com>
2018-04-11 08:59:01 +0800

    ...

--- src/main/java/MySpace/A3.java
+++ src/main/java/MySpace/A3.java
@@ -1,5 +1,8 @@
 package MySpace;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author: hyb
  * Date: Created in 2018/4/8 16:59
@@ -22,7 +25,34 @@ public class A3 {
         }
     }
     public static void main(String[] args){
-        int plus = plus(5);
-        System.out.println(plus);
+//        int plus = plus(5);
+//        System.out.println(plus);
+
+        String[] id1 = {"1111","aaaa","ddddd"};
+        List<String> id2 = new ArrayList<String>();
+        id2.add("aaaa");
+        id2.add("1111");
+        id2.add("ddddd");
+
+        if (id2.containsAll(getArrList(id1)))
+        {
+            System.out.println(true);
+        }
+        else
+        {
+            System.out.println(false);
+        }
+
+
     }
+
+    private static List<String> getArrList(String[] arr) {
+        List<String> list = new ArrayList<String>();
+        for(String col : arr) {
+            list.add(col);
+        }
+        return list;
+    }
+
+
 }
