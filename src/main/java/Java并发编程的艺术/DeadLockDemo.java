20f074e1b0325b3826c876ce6de025b7889c8cae
黄小波 <cqyc520@126.com>
2018-04-23 15:29:18 +0800

    ...

---
+++ src/main/java/Java并发编程的艺术/DeadLockDemo.java
@@ -0,0 +1,41 @@
+package Java并发编程的艺术;
+
+/**
+ * 这段代码会引起死锁，使线程t1和线程t2互相等待对方释放锁。
+ */
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
