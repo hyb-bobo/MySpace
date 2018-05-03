ee2a8f6fe3c5ca0c11ed5c43d4594461008d8c66
黄小波 <cqyc520@126.com>
2018-05-03 08:52:17 +0800

    ...

--- src/main/java/Java多线程核心技术/MyListWait.java
+++ src/main/java/Java多线程核心技术/MyListWait.java
@@ -1,5 +1,7 @@
 package Java多线程核心技术;
 
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,4 +20,17 @@ public class MyListWait {
         return list.size();
     }
 
+    public static void main(String[] args){
+        String s = new String("");
+        System.out.println("程序外");
+        synchronized (s){
+            System.out.println("程序内,wait前");
+            try {
+                s.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("程序内,wait后");
+        }
+    }
 }
