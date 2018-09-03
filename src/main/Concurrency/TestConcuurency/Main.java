edb6d02cf4238d07cb3402467c4b0521112c1c79
hyb-bobo <31762573+hyb-bobo@users.noreply.github.com>
2018-09-03 17:30:51 +0800

    ..

--- src/main/Concurrency/TestConcuurency/Main.java
+++ src/main/Concurrency/TestConcuurency/Main.java
@@ -1,14 +1,32 @@
 package TestConcuurency;
 
+import sun.rmi.runtime.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args)
     {
-        boolean result = false;
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 200; i++) {
+            Runnable syncRunnable = new Runnable() {
+                @Override
+                public void run() {
+//                    Log.e(, Thread.currentThread().getName());
+                    System.out.println(Thread.currentThread().getName());
+                }
+            };
+            executorService.execute(syncRunnable);
+        }
+        /*boolean result = false;
         try {
             result = ApplicationStartupUtil.checkExternalServices();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("External services validation completed !! Result was :: "+ result);
+        System.out.println("External services validation completed !! Result was :: "+ result);*/
     }
 }
